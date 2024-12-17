659a7ae6ca61df757e6a2c2521d5adf083a67b94
Lucky Verma <idonthaveemail1af@gmail.com>
2024-12-17 14:55:09 +0530

    Add user authentication to Navbar and include Cloudinary dependency

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,21 @@
+import { UserButton, useUser } from '@clerk/nextjs'; // Import Clerk's useUser hook
 import Link from 'next/link';
 
 export default function Navbar() {
+  const { user } = useUser(); // Get the user object
+
   return (
     <nav className='bg-blue-600 text-white px-4 py-2'>
       <div className='container mx-auto flex justify-between items-center'>
         <Link href='/' className='text-xl font-bold'>
           My Image App
         </Link>
-        <div className='space-x-4'>
+        <div className='space-x-4 flex items-center'>
           <Link href='/upload' className='hover:underline'>
             Upload
           </Link>
-          <Link href='/sign-in' className='hover:underline'>
-            Sign In
-          </Link>
-          <Link href='/sign-up' className='hover:underline'>
-            Sign Up
-          </Link>
+
+          <UserButton afterSignOutUrl='/sign-in' />
         </div>
       </div>
     </nav>
